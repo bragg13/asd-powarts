0fb0ad0d30b0cb031242d799d4013ed2acbc3250
Andrea <bragg003@gmail.com>
2020-12-10 00:13:02 +0100

    Implemented Dijkstra with parents, tested&working (?) on small input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <fstream>
 using namespace std;
 
+/*
+ * TODO: se un nodo è troppo distante non va guardato
+*/
 
 struct edge {
     int to;
@@ -20,26 +24,69 @@ int P;                      //city of Powarts
 vector<citta> graph;
 
 void getInput(){
-    //Input for N M P
     ifstream in("input.txt");
-    in >> N >> M >> P;
+    in >> N >> M >> P;                                                                      //input for N M P
     graph.resize(N);
 
-    //Input for edges (bidirectional)
-    //Metto i nodi nell'indice corretto
-    for(int i=0; i<M; i++){
-        int a, b, w;
-        edge e;
-        in >> a >> b >> w;
-        e.weight = w;
-        
-        // a--[w]--b
-        e.to = b;
-        graph[a].adj.push_back(e);
 
-        // b--[w]--a
-        e.to = a;
-        graph[b].adj.push_back(e);
+   for(int i=0; i<M; i++){ 
+       int a, b, w;
+       in >> a >> b >> w;
+
+       edge e;
+
+       e.weight = w;
+       
+       // a--[w]--b
+       e.to = b;
+       graph[a].adj.push_back(e);
+
+       // b--[w]--a
+       e.to = a;
+       graph[b].adj.push_back(e);
+   }
+}
+
+void dijkstra(){
+    priority_queue< pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>> > q;      //priority queue con min heap (il minore è il top)
+
+    vector<int> dist(N, 10000); //not sure 10000 is correct, maybe 10001?                   //vettore che tenga le distanze di ogni nodo dal root
+
+    q.push(make_pair(0, P));                                                                //pusho in prqueue il nodo di partenza, con distanza 0 (va messo prima 0 perche tiene ordinata la queue)
+    dist[P] = 0;                                                                            //la distanza da root a root è 0 (riporto quel che ho messo nella queue appena sopra)
+
+    vector<int> parent(N, 10000);                                                           //vettore che tenga i parent di ogni nodo
+    parent[P] = P;                                                                          //il nodo root non ha padre, aka e' se stesso?
+
+    while(!q.empty()){                                                                      //while queue is not empty
+        int nodo = q.top().second;   q.pop();                                               //poppo l'indice del nodo (aka anche il numero del nodo effettivo) che ha distanza minore finora
+
+        for(int i=0; i<graph[nodo].adj.size(); i++){                                        //itero tra gli adiacenti del nodo
+
+            int nodo_adj = graph[nodo].adj[i].to;                                           //prendo il nodo adiacente iterato e il suo peso
+            int nodo_adj_weight = graph[nodo].adj[i].weight;
+
+            if(dist[nodo_adj] > dist[nodo]+nodo_adj_weight){                                //se questo è un cammino piu veloce per arrivare a nodo_adj
+
+                dist[nodo_adj] = dist[nodo]+nodo_adj_weight;                                //segno la nuova distanza
+                
+                parent[nodo_adj] = nodo;                                                    //tengo traccia del parent da cui arrivo a nodo_adj
+
+                q.push(make_pair(dist[nodo_adj], nodo_adj));                                //metto il queue la coppia con la distanza di questo nodo
+            }
+        }    
+    }
+    
+
+    for(int i=0; i<N; i++){
+        cout << P << " - " << i << " = " << dist[i] << endl;
+        cout << " path: " << i << "-";
+        int j=i;
+        while(!parent[j]==P){
+            cout << parent[j] << "-";
+            j = parent[j];
+        }
+        cout << P << endl;
     }
 }
 
@@ -54,7 +101,12 @@ void printGraph(){
 
 int main(){
     getInput();
-    printGraph();
+    // printGraph();
+   
+    cout << endl << endl;
+
+    dijkstra();
+
     return 0;
 }
 
